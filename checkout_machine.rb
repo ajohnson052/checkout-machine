70d029bfae95f0efc85c5c1449dce7461a3ece92
Alexis Johnson <alexis.johnson052@gmail.com>
2016-12-07 21:44:30 -0500

    separate out tally keeper

--- checkout_machine.rb
+++ checkout_machine.rb
@@ -1,23 +1,22 @@
 require_relative 'scanner'
 require_relative 'item'
 require_relative 'bogo_machine'
+require_relative 'tally_keeper'
 require 'pry'
 
 class CheckoutMachine
-  attr_accessor :items_scanned
-
   def initialize
+    @tally_keeper = TallyKeeper.new
     @items_scanned = {}
     @balance = 0
   end
 
   def scan(sku)
-    item = Scanner.new.get_item(sku)
-    update_items(item)
+    @tally_keeper.scan(sku)
   end
 
   def total
-    items_scanned.each do |_k, item|
+    @tally_keeper.items_scanned.each do |_k, item|
       final_price = bonus_card_scanned? ? item[:discounted_price] : item[:price]
       @balance += final_price * item[:number]
     end
@@ -28,23 +27,10 @@ class CheckoutMachine
   private
 
   def apply_discount
-    @balance -= BOGOMachine.new(items_scanned).discount
-  end
-
-  def update_items(item)
-    number = number_of(item.sku) + 1
-    items_scanned[item.sku] = {
-      number: number,
-      price: item.price,
-      discounted_price: item.discounted_price
-    }
+    @balance -= BOGOMachine.new(@tally_keeper.items_scanned).discount
   end
 
   def bonus_card_scanned?
-    items_scanned.has_key?(000)
-  end
-
-  def number_of(sku)
-    items_scanned.has_key?(sku) ? items_scanned[sku][:number] : 0
+    @tally_keeper.items_scanned.has_key?(000)
   end
 end
---
+++ tally_keeper.rb
@@ -0,0 +1,27 @@
+class TallyKeeper
+  attr_accessor :items_scanned
+
+  def initialize
+    @items_scanned = {}
+  end
+
+  def scan(sku)
+    item = Scanner.new.get_item(sku)
+    update_items(item)
+  end
+
+  private
+
+  def update_items(item)
+    number = number_of(item.sku) + 1
+    items_scanned[item.sku] = {
+      number: number,
+      price: item.price,
+      discounted_price: item.discounted_price
+    }
+  end
+    
+  def number_of(sku)
+    items_scanned.has_key?(sku) ? items_scanned[sku][:number] : 0
+  end
+end
