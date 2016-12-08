d94a104ebb94f9fab8c6699413811a7041ff3164
Alexis Johnson <alexis.johnson052@gmail.com>
2016-12-07 21:58:23 -0500

    separate out balance keeper

---
+++ balance_keeper.rb
@@ -0,0 +1,26 @@
+class BalanceKeeper
+
+  def initialize(items_scanned)
+    @items_scanned = items_scanned
+    @balance = 0
+  end
+
+  def total
+    @items_scanned.each do |_k, item|
+      final_price = bonus_card_scanned? ? item[:discounted_price] : item[:price]
+      @balance += final_price * item[:number]
+    end
+    apply_discount if bonus_card_scanned?
+    @balance
+  end
+
+  private
+
+  def apply_discount
+    @balance -= BOGOMachine.new(@items_scanned).discount
+  end
+
+  def bonus_card_scanned?
+    @items_scanned.has_key?(000)
+  end
+end
--- checkout_machine.rb
+++ checkout_machine.rb
@@ -2,13 +2,12 @@ require_relative 'scanner'
 require_relative 'item'
 require_relative 'bogo_machine'
 require_relative 'tally_keeper'
+require_relative 'balance_keeper'
 require 'pry'
 
 class CheckoutMachine
   def initialize
     @tally_keeper = TallyKeeper.new
-    @items_scanned = {}
-    @balance = 0
   end
 
   def scan(sku)
@@ -16,21 +15,6 @@ class CheckoutMachine
   end
 
   def total
-    @tally_keeper.items_scanned.each do |_k, item|
-      final_price = bonus_card_scanned? ? item[:discounted_price] : item[:price]
-      @balance += final_price * item[:number]
-    end
-    apply_discount if bonus_card_scanned?
-    @balance
-  end
-
-  private
-
-  def apply_discount
-    @balance -= BOGOMachine.new(@tally_keeper.items_scanned).discount
-  end
-
-  def bonus_card_scanned?
-    @tally_keeper.items_scanned.has_key?(000)
+    BalanceKeeper.new(@tally_keeper.items_scanned).total
   end
 end
