3e996c0f2a0a36584d28821c4ce74d622367dedd
Alexis Johnson <alexis.johnson052@gmail.com>
2016-12-14 07:53:27 -0500

    BOGOMachine take an item and a quantity

--- balance_keeper.rb
+++ balance_keeper.rb
@@ -8,17 +8,18 @@ class BalanceKeeper
   def total
     @items_scanned.each do |sku|
       item = ItemCatalog.get_item(sku)
+      quantity = @scanner.number_of(sku)
       final_price = bonus_card_scanned? ? item.discounted_price : item.price
-      @balance += final_price * @scanner.number_of(sku)
+      @balance += final_price * quantity
+      apply_discount(item, quantity) if bonus_card_scanned?
     end
-    apply_discount if bonus_card_scanned?
     @balance
   end
 
   private
 
-  def apply_discount
-    @balance -= BOGOMachine.new(@scanner).discount
+  def apply_discount(item, quantity)
+    @balance -= BOGOMachine.new(item: item, quantity: quantity).discount
   end
 
   def bonus_card_scanned?
--- bogo_machine.rb
+++ bogo_machine.rb
@@ -1,12 +1,11 @@
 class BOGOMachine
-  def initialize(scanner)
-    @scanner = scanner
-    @items_scanned = scanner.items_scanned
+  def initialize(hash)
+    @item = hash[:item]
+    @quantity = hash[:quantity]
   end
 
   def discount
-    return 0 unless @items_scanned.include?(123)
-    item = ItemCatalog.get_item(123)
-    item.price * (@scanner.number_of(123)/3).floor
+    return 0 unless @item.sku == 123
+    @item.price * (@quantity/3).floor
   end
 end
