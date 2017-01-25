12ba794d39e8d51d1626fe6e6cdb1b878e2c028d
Alexis Johnson <alexis.johnson052@gmail.com>
2017-01-24 20:14:30 -0500

    remove old dependency

--- checkout_machine.rb
+++ checkout_machine.rb
@@ -2,7 +2,6 @@ require_relative 'item_catalog'
 require_relative 'item'
 require_relative 'bogo_machine'
 require_relative 'scanner'
-require_relative 'balance_keeper'
 require 'pry'
 
 class CheckoutMachine
